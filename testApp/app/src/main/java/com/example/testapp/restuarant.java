d91ea627f2b4a23bb682f584cbf4e7341c3531c5
Cuthbert-cloud <56514564+Cuthbert-cloud@users.noreply.github.com>
2021-01-29 16:18:05 +0000

    Flask server added and connected

--- testApp/app/src/main/java/com/example/testapp/restuarant.java
+++ testApp/app/src/main/java/com/example/testapp/restuarant.java
@@ -3,13 +3,14 @@ package com.example.testapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 import android.widget.*;
 
 import java.io.IOException;
 import okhttp3.*;
 
 public class restuarant extends AppCompatActivity {
-    private String url = "http://10.0.2.2:5000/";
+    private String url = "http://" + "10.0.2.2" + ":" + 5000 + "/";
     private String postBodyString;
     private MediaType mediaType;
     private RequestBody requestBody;
@@ -19,6 +20,11 @@ public class restuarant extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.restuarant);
+        connect = findViewById(R.id.table1);
+       /* connect.setOnClickListener(v -> {
+            postRequest("your message: ", url);
+        });*/
+        postRequest("your message: ",url);
 
     }
     private RequestBody buildRequestBody(String msg) {
@@ -39,25 +45,19 @@ public class restuarant extends AppCompatActivity {
         okHttpClient.newCall(request).enqueue(new Callback() {
             @Override
             public void onFailure(final Call call, final IOException e) {
-                runOnUiThread(new Runnable() {
-                    @Override
-                    public void run() {
-                        Toast.makeText(restuarant.this, "Something went wrong:" + " " + e.getMessage(), Toast.LENGTH_SHORT).show();
-                        call.cancel();
-                    }
+                runOnUiThread(() -> {
+                    Toast.makeText(restuarant.this, "Something went wrong:" + " " + e.getMessage(), Toast.LENGTH_SHORT).show();
+                    call.cancel();
                 });
             }
 
             @Override
             public void onResponse(Call call, final Response response) throws IOException {
-                runOnUiThread(new Runnable() {
-                    @Override
-                    public void run() {
-                        try {
-                            Toast.makeText(restuarant.this, response.body().string(), Toast.LENGTH_LONG).show();
-                        } catch (IOException e) {
-                            e.printStackTrace();
-                        }
+                runOnUiThread(() -> {
+                    try {
+                        Toast.makeText(restuarant.this, response.body().string(), Toast.LENGTH_LONG).show();
+                    } catch (IOException e) {
+                        e.printStackTrace();
                     }
                 });
             }
