b468df8968dde6160246664710040b5aa2d6b642
Cuthbert-cloud <56514564+Cuthbert-cloud@users.noreply.github.com>
2021-02-02 12:53:41 +0000

    Update routing_server.py

--- testApp/app/src/Server/routing_server.py
+++ testApp/app/src/Server/routing_server.py
@@ -4,6 +4,30 @@ app = Flask(__name__)
 
 @app.route('/', methods=['GET','POST'])
 def test():
-    return "Success"
+    return "Connected"
+
+@app.route('/table', methods=['GET','POST'])
+def table():
+    return "Table"
+
+@app.route('/office', methods=['GET','POST'])
+def office():
+    return "Office"
+
+@app.route('/bar', methods=['GET','POST'])
+def bar():
+    return "Bar"
+
+@app.route('/Toilet', methods=['GET','POST'])
+def toilet():
+    return "Toilet"
+
+@app.route('/Kitchen', methods=['GET','POST'])
+def toilet():
+    return "Kitchen"
+
+@app.route('/exit_door', methods=['GET','POST'])
+def toilet():
+    return "Exit door"
 
 app.run(host="0.0.0.0", port=5000, debug=True)
