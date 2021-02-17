5b8a33d0ce68896719ad19f0dd2c27b68e27e57b
Cuthbert-cloud <56514564+Cuthbert-cloud@users.noreply.github.com>
2021-02-17 19:20:07 +0000

    Update Node.java

--- testApp/app/src/main/java/com/example/testapp/Node.java
+++ testApp/app/src/main/java/com/example/testapp/Node.java
@@ -1,4 +1,25 @@
 package com.example.testapp;
 
 public class Node {
+    int x;
+    int y;
+    String id;
+
+    public Node(String id, int x, int y) {
+        this.id = id;
+        this.x = x;
+        this.y = y;
+    }
+
+    public int getX() {
+        return this.x;
+    }
+
+    public int getY() {
+        return this.y;
+    }
+
+    public String getId() {
+        return this.id;
+    }
 }
